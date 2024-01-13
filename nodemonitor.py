96bb4a92f739864498f69085f74b7005ee999aeb
chatgpt420 <markcampana@gmail.com>
2024-01-13 20:56:29 +1100

    modified:   Implemented logging

--- nodemonitor.py
+++ nodemonitor.py
@@ -48,36 +48,41 @@ try:
 
         # Call the monitor class which polls resource sensors
         monitor = Sys(afterBoot)
-        print(F"CPU Average: {monitor.cpu10min}%\tCombined Memory Usage: {monitor.mem}%\tDisk Usage: {monitor.disk}%")
-
 
         # Monitor CPU usage which is averaged over 10 minutes
         # Allow 15 minutes before first alert is sent to allow system to stabilize
         if monitor.cpu10min > 95 and monitor.cpu10minAlarm == False and monitor.uptime > 900:
             monitor.cpuAlarmToggle()
+            log('nodemonitor','CPU usage high')
             alert('cpu 10 min average high', monitor.cpu10min)
         elif monitor.cpu10min < 80 and monitor.cpu10minAlarm:
+            log('nodemonitor','CPU usage has returned to normal')
             monitor.cpuAlarmToggle()
             alert('cpu restored', monitor.cpu10min)
 
         # Monitor free memory - (VM % + Swap %) / 2
         if monitor.mem > 80 and monitor.memAlarm == False:
+            log('nodemonitor','Combined memory is high')
             monitor.memAlarmToggle()
             alert('combined memory high', monitor.mem)
         elif monitor.mem < 70 and monitor.memAlarm:
+            log('nodemonitor','Combined memory has returned to normal')
             monitor.memAlarmToggle()
             alert('memory restored', monitor.mem)
 
         # Monitor free disk space
         if monitor.disk > 95 and monitor.diskAlarm == False:
+            log('nodemonitor','Disk usage is high')
             monitor.diskAlarmToggle()
             alert('disk usage high', monitor.disk)
         elif monitor.disk < 85 and monitor.diskAlarm:
+            log('nodemonitor','Disk usage returned to normal')
             monitor.diskAlarmToggle()
             alert('disk usage restored', monitor.disk)
         
         # Before combined free memory runs out, send an email alert and reboot immediately
         if monitor.mem > 96:
+            log('nodemonitor','Combined memory critical, rebooting')
             alert('oom, rebooting', monitor.mem)
             os.system("reboot")
 
